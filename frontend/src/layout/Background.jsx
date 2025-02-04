d08d3409f57b61c935e44869bf9f3155f04f6bf2
Zyrus R. Alvez <zyrusalvez13@gmail.com>
2025-02-04 17:25:14 +0800

    update image

--- frontend/src/layout/Background.jsx
+++ frontend/src/layout/Background.jsx
@@ -2,7 +2,7 @@ import background from "../assets/background.png"
 
 const Background = ({opacity}) => {
   return ( 
-    <div className="w-full h-screen -z-50 absolute flex flex-col bg-[url(assets/background.png)]">
+    <div className="w-full h-screen -z-50 absolute flex flex-col bg-[url(assets/background.png)] bg-cover bg-bottom bg-no-repeat">
       <div className={`h-[140px] ${opacity ? "" : "bg-white opacity-50"}`}></div>
       <div className={`${opacity ? "bg-white opacity-50" : ""} h-full`}></div>
     </div>
